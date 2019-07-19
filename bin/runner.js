f017cd786d4cf91e79eda5cc669cda3a1400388c
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-07-18 21:34:54 -0700

    added s3 output support

--- bin/runner.js
+++ bin/runner.js
@@ -1,12 +1,14 @@
 const _ = require('the-lodash');
 const Promise = require('the-promise');
+const dateFormat = require('dateformat');
 
 const optionDefinitions = [
     { name: 'instanceId', type: String },
     { name: 'region', type: String },
     { name: 'profile', type: String },
     { name: 'command', type: String },
-    { name: 'comment', type: String, optional: true }
+    { name: 's3Bucket', type: String, optional: true },
+    { name: 'task', type: String, optional: true }
 ]
 
 const ArgParser = require('../lib/args');
@@ -15,7 +17,22 @@ const options = ArgParser(optionDefinitions)
 var AWSClient = require('../lib/aws-client');
 var awsClient = new AWSClient(options);
 
-return awsClient.executeSSMShellCommand(options.instanceId, options.command, options.comment)
+var params = {
+
+}
+
+if (options.s3Bucket) {
+    params.OutputS3BucketName = options.s3Bucket;
+    params.OutputS3KeyPrefix = dateFormat(new Date, "yyyy-mm-dd");
+    if (options.task) {
+        params.OutputS3KeyPrefix = params.OutputS3KeyPrefix + '/' + options.task;
+    }
+}
+if (options.task) {
+    params.Comment = options.task;
+}
+console.log(options);
+return awsClient.executeSSMShellCommand(options.instanceId, options.command, params)
     .then(data => {
         // console.log(data);
         console.log(`Status: ${data.Status}`);
